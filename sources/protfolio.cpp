fa622652655eb8c91ae24ee807f314599b420a5e
Bulle <bulle.mostovoi@gmail.com>
2025-05-04 18:59:52 +0200

    updated caustics

--- sources/protfolio.cpp
+++ sources/protfolio.cpp
@@ -8,11 +8,17 @@
 #include <ctime>
 
 void caustics() {
-    int SEED = 12;
+    int SEED = 8;
     std::srand(SEED);
-    BW_Image im = Voronoi(512,512,40);
+    if (SEED == 0)
+        srand(time(NULL));
+    int width = 1024;
+    int height = 1024;
+    BW_Image vor1 = Voronoi(width, height,80) * 1;
+    BW_Image caustiques_bw = vor1.apply_fun(add_offset_then_exp);
     std::vector<unsigned char> thresholds = {0, 1, 150, 255};
     std::vector<Color3> colors = {Color3::fromHex("#006dd5"), Color3::fromHex("#006dd5"),Color3::fromHex("ffffff"),  Color3::fromHex("ffffff")};
-    Color_Image grad = gradient(im * 0.7, thresholds, colors);
-    grad.save("../portfolio/caustics.png");
+    Color_Image grad = gradient(caustiques_bw, thresholds, colors);
+    vor1.save("../output1.png");
+    grad.save("../output2.png");
 }

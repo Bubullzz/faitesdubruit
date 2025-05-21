cf16de9162aca85fbd428d03c5bd23f7195fc978
Luca Descrambes <luca.descrambes@epita.fr>
2025-05-21 21:37:21 +0000

    added portfolio start

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@
 
 
 int main() {
+    std::cout << "Generating caustics..." << std::endl;
+    caustics();
+    std::cout << "Generating giraffe..." << std::endl;
+    giraffe();
+    std::cout << "Generating game map..." << std::endl;
+    game_map();
+    std::cout << "Generating fire..." << std::endl;
     fire();
+    std::cout << "Generating biome map..." << std::endl;
+    colored_map();
 }
 
